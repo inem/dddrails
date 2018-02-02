74eacd90f97736dfe0eee53ffa2f651945bab2d3
Ivan Nemytchenko <nemytchenko@gmail.com>
2018-02-02 20:52:29 +0100

    fixes existing tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
   scope module: :web do
+    get 'welcome/index'
+
     resources :articles do
       scope module: :articles do
         resources :comments
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
 
     root 'welcome#index'
   end
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
