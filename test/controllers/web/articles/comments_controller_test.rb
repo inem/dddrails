f36677dea907cac38f78f836af8db19674aa5ec1
Ivan Nemytchenko <nemytchenko@gmail.com>
2018-02-03 11:25:59 +0100

    tests for comments_controller

--- test/controllers/web/articles/comments_controller_test.rb
+++ test/controllers/web/articles/comments_controller_test.rb
@@ -1,8 +1,22 @@
 require 'test_helper'
 
 class Web::Articles::CommentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    # get welcome_index_url
-    # assert_response :success
+  setup do
+    @article = articles(:one)
+  end
+
+  test "should create" do
+    params = {commentor: "Vovchik", body: "heyhey"}
+    post article_comments_url(@article.id), params: {article_comment: params}
+
+    assert(@article.comments.find_by(params).persisted?)
+  end
+
+  test "should destroy" do
+    comment = article_comments(:one)
+    delete article_comment_url(comment.article.id, comment.id)
+
+    @article = comment.article
+    assert { @article.comments.find_by(id: comment.id).nil? }
   end
 end
