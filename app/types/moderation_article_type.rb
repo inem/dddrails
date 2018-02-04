522d96c2f836836232395b850b3f3b6f54d887c3
Ivan Nemytchenko <nemytchenko@gmail.com>
2018-02-04 06:53:48 +0100

    added more permits inside type

--- app/types/moderation_article_type.rb
+++ app/types/moderation_article_type.rb
@@ -1,7 +1,7 @@
 class ModerationArticleType < Article
   include ApplicationType
 
-  permit :category_id
+  permit :title, :text, :category_id, :state_event
 
   validates :category_id, presence: true
 end
