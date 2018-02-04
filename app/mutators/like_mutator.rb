23f11f67a6456ba5ca33e4c87f1437285a5abb57
Ivan Nemytchenko <nemytchenko@gmail.com>
2018-02-04 17:21:39 +0100

    like -> @like

--- app/mutators/like_mutator.rb
+++ app/mutators/like_mutator.rb
@@ -4,11 +4,11 @@ class LikeMutator
       likes_in_current_hour = Article::Comment::Like.where(created_at: 1.hour.ago..Time.current, comment: comment).count
 
       if likes_in_current_hour < 5
-        like = comment.likes.build(article: comment.article)
-        like.save!
+        @like = comment.likes.build(article: comment.article)
+        @like.save!
       end
 
-      Maybe(like)
+      Maybe(@like)
     end
   end
 end
