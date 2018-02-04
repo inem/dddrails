2d11d7ea43df7de3ac43af0e98997645ecb6a3d3
Ivan Nemytchenko <nemytchenko@gmail.com>
2018-02-04 19:21:14 +0100

    resource_ stuff in application controllers

--- app/controllers/web/articles/application_controller.rb
+++ app/controllers/web/articles/application_controller.rb
@@ -1,2 +1,5 @@
 class Web::Articles::ApplicationController < Web::ApplicationController
+  def resource_article
+    @article ||= Article.find(params[:article_id])
+  end
 end
--- app/controllers/web/articles/comments/application_controller.rb
+++ app/controllers/web/articles/comments/application_controller.rb
@@ -1,2 +1,5 @@
 class Web::Articles::Comments::ApplicationController < Web::Articles::ApplicationController
+  def resource_comment
+    @comment ||= resource_article.comments.find(params[:comment_id])
+  end
 end
