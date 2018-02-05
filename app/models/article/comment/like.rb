70e08db29ff92abbca1ecb95f452804717f13e2e
Ivan Nemytchenko <nemytchenko@gmail.com>
2018-02-05 20:38:00 +0100

    cleanup

--- app/models/article/comment/like.rb
+++ app/models/article/comment/like.rb
@@ -3,16 +3,4 @@ class Article::Comment::Like < ApplicationRecord
   belongs_to :article
 
   scope :within_one_hour, -> { where('created_at > ?', 1.hour.ago) }
-
-  # Так низя. Атата! Потомушто лайку низя знать про другие лайки
-
-  # validate :no_more_than_five_likes
-
-  # def no_more_than_five_likes
-  #   count = Article::Comment::Like.where(created_at: 1.hour.ago..Time.current, comment: @comment).count
-
-  #   if likes_in_current_hour > 5
-  #     errors.add_to_base("Too many likes per hour!!!")
-  #   end
-  # end
 end
